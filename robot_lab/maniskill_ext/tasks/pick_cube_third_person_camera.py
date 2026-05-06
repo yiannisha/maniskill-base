ea8fcb1c15dfd5148b94156210023a7ac25af0a5
Yiannis Hadjiyianni <yiannis.hadjiyianni@toolsforhumanity.com>
2026-05-06 16:29:02 -0700

    feat: add the wrist camera

--- robot_lab/maniskill_ext/tasks/pick_cube_third_person_camera.py
+++ robot_lab/maniskill_ext/tasks/pick_cube_third_person_camera.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import numpy as np
+import sapien
 from mani_skill.envs.tasks.tabletop.pick_cube import PickCubeEnv
 from mani_skill.sensors.camera import CameraConfig
 from mani_skill.utils import sapien_utils
@@ -9,7 +10,7 @@ from mani_skill.utils.registration import register_env
 
 @register_env("PickCubeThirdPersonCamera-v0", max_episode_steps=50)
 class PickCubeThirdPersonCameraEnv(PickCubeEnv):
-    """PickCube with an additional fixed third-person sensor camera."""
+    """PickCube with additional third-person and Panda wrist sensor cameras."""
 
     @property
     def _default_sensor_configs(self):
@@ -29,4 +30,21 @@ class PickCubeThirdPersonCameraEnv(PickCubeEnv):
                 100,
             )
         )
+        wrist_mount = self.agent.robot.links_map.get("panda_hand")
+        if wrist_mount is not None:
+            sensor_configs.append(
+                CameraConfig(
+                    "wrist_camera",
+                    sapien.Pose(
+                        p=[0.0464982, -0.0200011, 0.0360011],
+                        q=[0.0, 0.70710678, 0.0, 0.70710678],
+                    ),
+                    128,
+                    128,
+                    np.pi / 2,
+                    0.01,
+                    100,
+                    mount=wrist_mount,
+                )
+            )
         return sensor_configs
